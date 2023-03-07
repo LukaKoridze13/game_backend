11a56576b6c4f07e677853c922c3bdcce2ff7ce3
LukaKoridze13 <lukakoridze13@gmail.com>
2023-03-08 00:55:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 import express from "express";
+const app = express();
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  },
-});
 app.use(cors());
 
-server.listen(3000, () => {
+const server = http.createServer(app);
+const PORT = 3001 || process.env.PORT;
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+io.on('connection',()=>{
+  console.log("User Connected")
+})
+server.listen(PORT, () => {
   console.log("listening on *:3000");
 });
