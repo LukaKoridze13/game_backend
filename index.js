9fc2e7738ce10cf9930ad212f57aa37e3faef7d1
LukaKoridze13 <lukakoridze13@gmail.com>
2023-03-07 22:52:52 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
 });
+app.use(cors());
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
